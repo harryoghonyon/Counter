91657fb904b489fbb5a7b9acabdcf34123d6615c
Harry Oghonyon <harrytones9@gmail.com>
2023-04-18 19:08:23 +0100

    ativate counter js code
    and add more control button breakpoint styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 const content = document.getElementsByTagName("body")[0];
 const lightModeIcon = document.querySelector(".light-mode-icon");
 const darkModeIcon = document.querySelector(".dark-mode-icon");
+const subtractButton = document.getElementById("subtract-button");
+const addButton = document.getElementById("addition-button");
 
 darkModeIcon.addEventListener("click", () => {
   content.classList.add("dark-mode");
@@ -8,4 +10,17 @@ darkModeIcon.addEventListener("click", () => {
 
 lightModeIcon.addEventListener("click", () => {
   content.classList.remove("dark-mode");
+});
+
+let counter = 0;
+
+document.getElementById("counter-txt").innerHTML = counter;
+
+subtractButton.addEventListener("click", () => {
+  counter -= 1;
+  document.getElementById("counter-txt").innerHTML = counter;
+});
+addButton.addEventListener("click", () => {
+  counter += 1;
+  document.getElementById("counter-txt").innerHTML = counter;
 });
